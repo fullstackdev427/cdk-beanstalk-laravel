65891b2e880285c13d051c6c0c036a7125af0329
fullstackdev427 <bear_star713@hotmail.com>
2021-10-11 17:22:16 +0900

    deploy new laravel project

--- lib/elbtest-stack.ts
+++ lib/elbtest-stack.ts
@@ -1,9 +1,66 @@
-import * as cdk from '@aws-cdk/core';
+import cdk = require('@aws-cdk/core');
+import elasticbeanstalk = require('@aws-cdk/aws-elasticbeanstalk');
+import s3assets = require('@aws-cdk/aws-s3-assets');
 
 export class ElbtestStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // Construct an S3 asset from the ZIP located from directory up.
+        const elbZipArchive = new s3assets.Asset(this, 'MyElbAppZip', {
+            //path: `${__dirname}/../app.zip`,
+            path: `${__dirname}/../laravel-default.zip`,
+        });
+
+        const appName = 'MyApp';
+        const app = new elasticbeanstalk.CfnApplication(this, 'Application', {
+            applicationName: appName,
+        });
+
+        // Example of some options which can be configured
+        const optionSettingProperties: elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
+            {
+                namespace: 'aws:autoscaling:launchconfiguration',
+                optionName: 'InstanceType',
+                value: 't3.small',
+            },
+            {
+                namespace: 'aws:autoscaling:launchconfiguration',
+                optionName: 'IamInstanceProfile',
+                // Here you could reference an instance profile by ARN (e.g. myIamInstanceProfile.attrArn)
+                // For the default setup, leave this as is (it is assumed this role exists)
+                // https://stackoverflow.com/a/55033663/6894670
+                value: 'aws-elasticbeanstalk-ec2-role',
+            },
+            {
+                namespace: 'aws:autoscaling:launchconfiguration',
+                optionName: 'Document root',
+                value: '/public',
+            },
+        ];
+
+        // Create an app version from the S3 asset defined above
+        // The S3 "putObject" will occur first before CF generates the template
+        const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+            applicationName: appName,
+            sourceBundle: {
+                s3Bucket: elbZipArchive.s3BucketName,
+                s3Key: elbZipArchive.s3ObjectKey,
+            },
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
+            //environmentName: 'PHP-8-Env',
+            environmentName: 'PHP-74-Env',
+            applicationName: app.applicationName || appName,
+            //solutionStackName: '64bit Amazon Linux 2 v3.3.6 running PHP 8.0',
+            solutionStackName: '64bit Amazon Linux 2 v3.3.6 running PHP 7.4',
+            optionSettings: optionSettingProperties,
+            // This line is critical - reference the label created in this same stack
+            versionLabel: appVersionProps.ref,
+        });
+        // Also very important - make sure that `app` exists before creating an app version
+        appVersionProps.addDependsOn(app);
+    }
 }
